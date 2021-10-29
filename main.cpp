4bbe4fe886d843a4b0644c05498994cce91a62cf
Jasmine Meyer <45775227+jmeyer27@users.noreply.github.com>
2021-10-29 22:13:51 +0000

    Added Makefile, Functionality in main.cpp that potentially could accept input filename and other information from command line

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 //reading resource is page 272
 #include "Scheduler.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
   //Print basic information about the program
 	cout << "CS 433 Programming assignment 3\n"
 	"Author: Birhane Diarra and Jasmine Meyer\n"
@@ -13,19 +14,31 @@ int main() {
 	"Description : \n"
 	"=================================" << endl;
   Scheduler scheduler;  
+  
 
 
 
-//todo learn how to make console commands work in cpp
-//trying this?? supposed to respond to input "./filename textfilename (potential number for rr)""
-//cin next input on same line = filename
-//cin optional input for rr number
+  //todo learn how to make console commands work in cpp
+  //supposed to respond to input "./filename textfilename (potential number for rr)"
+  string inFile = "";
+  int RRnumber = 10; //setting default to 10
+  if(argc == 3){
+    inFile = argv[1];
+    //RRnumber = (int) argv[2]; TODO idk I'll fix this later or something~
+  }
+  else{
+    cout << "Incorrect format." << endl;
+  }
+  //cin next input on same line = filename-- might not be necessary anymore
+  //cin optional input for rr number-- might not be necessary anymore
 
 
 
-//switch console commands with one of three options
-//run appropriate option
-//make sure stuff is printed out
+//switch console commands with one of three options (unless ./filename runs file by default, then this step is not included)
+
+//run appropriate option (unless it runs itself with ./filename?)
+
+//make sure stuff is printed out (probably will be included in each class)
 
 
 
